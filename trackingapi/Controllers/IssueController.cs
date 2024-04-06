9898c37d1717d0976eae17aae0d7564807717ed2
kennethMurithi <kennthmmuriithi@gmail.com>
2024-04-06 14:09:46 +0300

    Add project files.

---
+++ trackingapi/Controllers/IssueController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using trackingapi.Data;
+using trackingapi.Models;
+
+namespace trackingapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : ControllerBase
+    {
+        private readonly IssueDbContext _context;
+
+        public IssueController(IssueDbContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<IEnumerable<Issue>> Get()  
+            => await _context.Issues.ToListAsync(); 
+
+        [HttpGet]
+        public 
+    }
+}
